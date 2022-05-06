efc88653bca171b2d33dcf8f8cc65c69df57ebe7
magura13 <tiagolelis_@hotmail.com>
2022-05-06 10:14:00 -0300

    Arquitetura do projeto

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
-const routes = require("./src/routes/routes.js")
-
 const app = express();
-const port = process.env.PORT || 3000;
+
+// const routes = require("./src/routes/routes.js")
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded());
-app.use(routes);
+// app.use(routes);
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () =>
   console.log(`Servidor rodando em http://localhost:${port}`)
@@ -87,5 +88,3 @@ app.listen(port, () =>
 
 //   res.redirect("/#cards");
 // });
-
-
